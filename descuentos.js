da6faf608ba9c26d56cd3cded46974634dda4a08
Israel Rios <brayanisrra1@gmail,com>
2021-08-16 17:24:33 -0500

    Creacion de la funcion

---
+++ descuentos.js
@@ -0,0 +1,17 @@
+// const precioOriginal = 330;
+// const descuento = 15;
+
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento)/100;
+
+    return("El precio con descuento es de: " + precioConDescuento);
+}
+
+// console.log({
+//     precioOriginal,
+//     descuento,
+//     porcentajePrecioConDescuento,
+//     precioConDescuento,
+// })
